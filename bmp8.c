6da3f500e2f37cb11354e05e7236204c94f4ef9e
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2025-03-21 11:19:00 +0100

    cooked

--- bmp8.c
+++ bmp8.c
@@ -1,15 +1,69 @@
 #include " bmp8.h"
-
-int bmp8_loadImage(const char * filename) {
-    FILE * file = fopen(filename, "rb");
+t_bmp8* bmp8_loadImage(const char* filename) {
+    FILE* file = fopen(filename, "rb");
     if (!file) {
         fprintf(stderr, "Error: Unable to open file %s\n", filename);
         return NULL;
-    } else {
-        printf("File opened sucessfully");
-}
-        return 0;
     }
+
+    unsigned char header[54];
+    if (fread(header, sizeof(unsigned char), 54, file) != 54) {
+        fprintf(stderr, "Error: Unable to read BMP header from file %s\n", filename);
+        fclose(file);
+        return NULL;
+    }
+
+    if (header[0] != 'B' || header[1] != 'M') {
+        fprintf(stderr, "Error: File %s is not a valid BMP file\n", filename);
+        fclose(file);
+        return NULL;
+    }
+
+    unsigned int width = *(unsigned int*)&header[18];
+    unsigned int height = *(unsigned int*)&header[22];
+    unsigned short colorDepth = *(unsigned short*)&header[28];
+    unsigned int dataSize = *(unsigned int*)&header[34];
+
+    if (colorDepth != 8) {
+        fprintf(stderr, "Error: BMP file %s is not an 8-bit grayscale image (color depth = %d)\n", filename, colorDepth);
+        fclose(file);
+        return NULL;
+    }
+
+    t_bmp8* image = (t_bmp8*)malloc(sizeof(t_bmp8));
+    if (!image) {
+        fprintf(stderr, "Error: Memory allocation failed for BMP structure\n");
+        fclose(file);
+        return NULL;
+    }
+
+    image->width = width;
+    image->height = height;
+    image->colorDepth = colorDepth;
+    image->dataSize = dataSize;
+
+    image->data = (unsigned char*)malloc(dataSize);
+    if (!image->data) {
+        fprintf(stderr, "Error: Memory allocation failed for BMP data\n");
+        free(image);
+        fclose(file);
+        return NULL;
+    }
+
+    unsigned int dataOffset = *(unsigned int*)&header[10];
+    fseek(file, dataOffset, SEEK_SET);
+
+    if (fread(image->data, sizeof(unsigned char), dataSize, file) != dataSize) {
+        fprintf(stderr, "Error: Unable to read BMP image data from file %s\n", filename);
+        free(image->data);
+        free(image);
+        fclose(file);
+        return NULL;
+    }
+
+    fclose(file);
+    return image;
+}
     
 void bmp8_saveImage(const char * filename, t_bmp8 * img) {
 if (!filename || !img || !img->data) {
